0483155c22c620b25f92f27283d435cf0b356530
mjankows <mjankows@WRKPCSI151>
2018-07-18 22:21:11 +0200

    add sample card

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(context) {
     return new MaterialApp(
-home: Scaffold(
-  appBar: AppBar(
-    title: Text('Flutter-Organizer')
-  ),
-),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter-Organizer')),
+        body:  Card(child: Column(children: <Widget>[
+          Image.asset('assets/bb.jpg'),
+          Text('Motivation')
+        ],),),
+      ),
     );
   }
 }
